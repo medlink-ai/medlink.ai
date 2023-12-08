33ecf1c3737c692287a1fbc5c80aa69db1a41de7
9teMare <wang.zihao@u.nus.edu>
2023-12-08 20:35:35 +0800

    add consumer creation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -45,18 +45,6 @@ export default function Home() {
                         <div>Some information</div>
                     </div>
                 </div>
-
-                <Button isIconOnly className="absolute right-0 top-[50%]" size="sm" onClick={() => setIsDealExpanded(!isDealExpanded)}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="16"
-                        width="10"
-                        viewBox="0 0 320 512"
-                        className={`${isDealExpanded && "rotate-180"} transition-all dark:fill-white`}
-                    >
-                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-                    </svg>
-                </Button>
             </ChainlinkFunctionContext.Provider>
         </main>
     );
