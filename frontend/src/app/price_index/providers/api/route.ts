1adc3064226a90fdf019cb80c4ce263a409db87e
9teMare <wang.zihao@u.nus.edu>
2023-12-08 22:07:06 +0800

    update route handlers and ChainlinkFunctionContext

--- frontend/src/app/price_index/providers/api/route.ts
+++ frontend/src/app/price_index/providers/api/route.ts
@@ -3,18 +3,25 @@ import axios from "axios";
 
 export async function POST(request: Request) {
     try {
-        const { product, budget } = await request.json();
+        const { product, budget, consumerAddress, subscriptionId } = await request.json();
 
         const functionRequest = await axios.post(`${process.env.API_BASE_URL}/api/chainlink-functions/function-request-provider`, {
             drug: product,
             amount: budget,
+            consumerAddress,
+            subscriptionId,
         });
 
         await functionRequest.data;
 
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
-        const functionResponse = await axios.post<ProviderDetail[]>(`${process.env.API_BASE_URL}/api/chainlink-functions/function-response-provider`);
+        const functionResponse = await axios.post<ProviderDetail[]>(
+            `${process.env.API_BASE_URL}/api/chainlink-functions/function-response-provider`,
+            {
+                consumerAddress,
+            }
+        );
 
         return Response.json(functionResponse.data);
     } catch (error) {
